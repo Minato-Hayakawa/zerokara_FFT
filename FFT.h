8b8c7d2b14de5dfddbe3fa886922d3a65fd112df
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2026-02-04 18:28:25 +0900

    [delete]

--- FFT.h
+++ FFT.h
@@ -4,14 +4,3 @@ typedef struct {
     double real;
     double image;
 } complex;
-
-typedef struct {
-    int head;
-    double data[N];
-} stack;
-
-void init(stack &data);
-stack push(stack &data);
-stack pop(stack &data);
-
-stack FFT(stack &data);
