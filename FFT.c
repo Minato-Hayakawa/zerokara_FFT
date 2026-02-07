a6bc433602978a7b080961e8252ce6fbd8fb8631
MH_Coder <Minato-Hayakawa@users.noreply.github.com>
2026-02-07 15:36:51 +0900

    [fix]

--- FFT.c
+++ FFT.c
@@ -39,6 +39,8 @@ stack *FFT(stack *in){
     stack *even = malloc(sizeof(stack));
     stack *odd = malloc(sizeof(stack));
     stack *out = malloc (sizeof(stack));
+    stack *even_result;
+    stack *odd_result;
 
     even -> head = in -> head / 2;
     odd -> head = in -> head / 2;
@@ -55,12 +57,8 @@ stack *FFT(stack *in){
         return out;
     }
     else if (in -> head != 1){
-        stack *even_result = FFT(even);
-        stack *odd_result = FFT(odd);
-        for (int i=0; i < even_result -> head; i++){
-            out -> data[i] = even_result -> data[i];
-            out -> data[i + out -> head / 2] = odd_result -> data[i];
-        }
+        even_result = FFT(even);
+        odd_result = FFT(odd);
     }
 
     free(even);
@@ -69,16 +67,19 @@ stack *FFT(stack *in){
         double weight_real = cos(-2 * M_PI * i / out -> head);
         double weight_image = sin(-2 * M_PI * i / out -> head);
         
-        double odd_real = out -> data[i + out -> head / 2].real * weight_real
-        - out -> data[i + out -> head / 2].image * weight_image;
-        double odd_image = out -> data[i + out -> head / 2].image * weight_real
-        + out -> data[i + out -> head / 2].real * weight_image;
-        double even_real = out -> data[i].real;
-        double even_image = out -> data[i].image;
+        double odd_real = odd_result -> data[i].real * weight_real
+        - odd_result -> data[i].image * weight_image;
+        double odd_image = odd_result -> data[i].image * weight_real
+        + odd_result -> data[i].real * weight_image;
+        double even_real = even_result -> data[i].real;
+        double even_image = even_result -> data[i].image;
 
         out -> data[i].real = even_real + odd_real;
         out -> data[i].image = even_image + odd_image;
         out -> data[i + out -> head / 2].real = even_real - odd_real;
         out -> data[i + out -> head / 2].image = even_image - odd_image;
-    }return out;
+    }
+    free(odd_result);
+    free(even_result);
+    return out;
 }
